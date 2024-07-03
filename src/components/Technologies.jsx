bf16e6f90386c31aa33554e7a822b23e067f1124
ijf03 <63137224+ijf03@users.noreply.github.com>
2024-07-03 17:14:24 +0800

    updated title to technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,7 +6,7 @@ import {FaNodeJs, FaPython, FaHtml5, FaCss3Alt } from 'react-icons/fa';
 const Technologies = () => {
     return (
         <div className='pb-24'>
-            <h2 className="my-20 text-center text-4xl">Technology</h2>
+            <h2 className="my-20 text-center text-4xl">Technologies</h2>
             <div className="flex flex-wrap items-center justify-center gap-4">
                 <div className="rounded-2xl border-4 border-mauve p-4">
                     <RiReactjsLine className="text-7xl text-peach"/>
